ac10ed2036fecf4fa4cc0423ea18a336eb0ee557
Ismafer Ferramentas <ismafercomercial@gmail.com>
2023-12-26 08:07:36 -0300

    "

--- src/pages/api/users/delete/index.ts
+++ src/pages/api/users/delete/index.ts
@@ -1,4 +1,3 @@
-import { UserBody } from '@/types/users';
 import { NextApiResponse } from 'next';
 import prisma from '@/lib/prisma';
 
@@ -6,7 +5,7 @@ export default async function handler(req: any, res: NextApiResponse) {
   if (req.method !== "POST") {
     res.status(400).send('BAD REQUEST')
   }
-  const body = req.body as UserBody
+  const body = req.body
 
   const deleteUser = await prisma?.user.delete({
     where: {
